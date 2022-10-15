10f62c334f3839e030d6a0eec62307b68d88fa1b
Ahmed mubarak <Ahmed.mubarak2024@gmail.com>
2022-10-15 12:43:43 +0200

    first commit

---
+++ src/main/java/com/mub/roomwebdemospring2essential/RoomWebDemoSpring2EssentialApplication.java
@@ -0,0 +1,13 @@
+package com.mub.roomwebdemospring2essential;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomWebDemoSpring2EssentialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomWebDemoSpring2EssentialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mub/roomwebdemospring2essential/RoomWebDemoSpring2EssentialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mub.roomwebdemospring2essential;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomWebDemoSpring2EssentialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
