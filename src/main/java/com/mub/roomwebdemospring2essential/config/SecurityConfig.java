efe96807e61393a39b3368006e839ff8c8aa17b2
Ahmed mubarak <Ahmed.mubarak2024@gmail.com>
2022-10-20 18:44:25 +0200

    spring data rest and Actuator added

--- src/main/java/com/mub/roomwebdemospring2essential/config/SecurityConfig.java
+++ src/main/java/com/mub/roomwebdemospring2essential/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.mub.roomwebdemospring2essential.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,8 +18,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/*.js", "/*.css").permitAll()
-                .antMatchers(HttpMethod.GET, "/*")
+                .antMatchers("/*.js", "/*.css").permitAll()
+                .antMatchers("/*", "/rest/*")
                 .permitAll().anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -29,9 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout().logoutUrl("/logout").logoutSuccessUrl("/").permitAll()
                 .and()
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-        ;
+                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
     }
 
     @Autowired
